a2b709ba6367ea047d13e68a5b99a79029b33919
Halkano Malise <kudezx@gmail.com>
2023-07-19 03:49:31 +0300

    updarted

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import Clients from "./components/Clients";
 import Home from "./components/Home";
 import CreateAndSell from "./components/CreateAndSell";
 import Choose from "./components/Choose";
+import MarketPlace from "./components/MarketPlaceComponent";
 
 function App() {
     return (
@@ -10,8 +11,8 @@ function App() {
             <Clients />
             <CreateAndSell />
             <Choose />
+            <MarketPlace />
         </div>
     );
 }
-
 export default App;
--- src/components/Choose.jsx
+++ src/components/Choose.jsx
@@ -1,11 +1,48 @@
 import React from "react";
 import { styled } from "styled-components";
 import choose from "../assets/choose.png";
+import Button from "./Button";
 
 const Choose = () => {
-    return <Section>Choose</Section>;
+    return (
+        <Section>
+            <div className="image">
+                <img src={choose} alt="choose" />
+            </div>
+            <div className="content">
+                <h2>Why SHould You Choose Our Website?</h2>
+                <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Adipisci possimus iure ex quia saepe veniam facere obcaecati
+                    earum, delectus recusandae! Eius officiis vel adipisci atque
+                    suscipit amet impedit eveniet quam! .
+                </p>
+                <Button text="Read More" blue />
+            </div>
+        </Section>
+    );
 };
 
 export default Choose;
 
-const Section = styled.section``;
+const Section = styled.section`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    gap: 3rem;
+    margin: 0 9rem;
+    margin-bottom: 5rem;
+    .content {
+        display: flex;
+        justify-content: center;
+        flex-direction: column;
+        gap: 3rem;
+        h2 {
+            font-size: 4rem;
+        }
+        p {
+            color: #7b7e86;
+            line-height: 2rem;
+        }
+    }
+`;
